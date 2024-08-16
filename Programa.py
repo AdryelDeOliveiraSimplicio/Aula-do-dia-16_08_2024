8fbfb89255f15616c41f19319b2177c62244bf35
AdryelDeOliveiraSimplicio <00001091914412sp@al.educacao.sp.gov.br>
2024-08-16 07:51:53 -0300

    Tarefa
    Programa de print o nome e também pensei em perguntar o nome de quem acionou o comando

---
+++ Programa.py
@@ -0,0 +1,13 @@
+# Programa que imprime o nome "Adryel" e também o nome e idade do usuário
+
+# Imprime o nome "Adryel"
+print("Olá, eu sou Adryel!")
+
+# Pergunta o nome do usuário
+nome_usuario = input("E você, qual é o seu nome? ")
+
+# Pergunta a idade do usuário
+idade_usuario = int(input("E qual é a sua idade? "))
+
+# Imprime o nome e idade do usuário
+print(f"Olá, {nome_usuario}! Você tem {idade_usuario} anos.")
